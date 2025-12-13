631278295b1b6bf5a85c65d1a86e7f8a463810d4
pablez <pablogr2928@gmail.com>
2025-12-12 23:40:16 -0400

    fix(screens): correct utils/constants import in RegisterScreen

--- src/screens/auth/RegisterScreen.js
+++ src/screens/auth/RegisterScreen.js
@@ -18,7 +18,7 @@ import { Picker } from '@react-native-picker/picker';
 import * as ImagePicker from 'expo-image-picker';
 import { Ionicons } from '@expo/vector-icons';
 import { useAuth } from '../../context/AuthContext';
-import { USER_ROLES, PASSENGER_TYPES } from '../utils/constants';
+import { USER_ROLES, PASSENGER_TYPES } from '../../utils/constants';
 import { isValidEmail, isValidPhone } from '../../utils/helpers';
 
 const { width } = Dimensions.get('window');
